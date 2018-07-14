70243d07f82cc4abd5786ee68c2e7f1ce5ff62b8
cool91788 <cool9035963@gmail.com>
2018-07-14 13:25:00 +0800

    Update license

--- src/install/java/accountcheck/command/AccountCheckCommand.java
+++ src/install/java/accountcheck/command/AccountCheckCommand.java
@@ -1,19 +1,21 @@
 /*
- * 	AccountCheck (半正版驗證) - A BungeeCord plugin
- *	Copyright (C) 2014  Install
+ * 	AccountCheck - A BungeeCord plugin
+ *	Copyright (C) (2014-2018)  Install
  *
- *   This program is free software: you can redistribute it and/or modify
+ *   This file is part of AccountCheck.
+ *
+ *   AccountCheck is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   AccountCheck is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with AccountCheck.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package install.java.accountcheck.command;
